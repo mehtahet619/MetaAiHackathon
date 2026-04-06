55fe9d56126c7038f385a4b88cdb5211e41965a6
mehtahet619 <mehtahet619@gmail.com>
2026-04-06 20:21:21 +0000

    Create server/app.py

---
+++ server/app.py
@@ -0,0 +1,22 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from openenv.core.env_server.http_server import create_app
+from models import TrafficControlAction, TrafficControlObservation
+from traffic_control_env_environment import TrafficControlEnvironment
+
+app = create_app(
+    TrafficControlEnvironment,
+    TrafficControlAction,
+    TrafficControlObservation,
+    env_name="traffic_control_env",
+    max_concurrent_envs=4,
+)
+
+def main(host="0.0.0.0", port=8000):
+    import uvicorn
+    uvicorn.run(app, host=host, port=port)
+
+if __name__ == "__main__":
+    main()
